9f60fb2ccf1d34903eafc4f05a931fb252187828
Talgat Amanbayev <amanbayev@gmail.com>
2016-05-23 17:37:59 +0600

    added some login UI

--- imports/ui/authorization/login/login.js
+++ imports/ui/authorization/login/login.js
@@ -1,2 +1,17 @@
 import {Meteor} from 'meteor/meteor';
 import './login.html';
+
+Template.login.events({
+  "submit #loginForm": function(e, t){
+     e.preventDefault();
+     var login = t.find('#loginEmailField').value;
+     var pw = t.find('#loginPasswordField').value;
+     Meteor.loginWithPassword(login,pw, function(err){
+      if (err) {
+        toastr.error(err.reason);
+      } else {
+        toastr.success('Добро пожаловать, '+Meteor.user().profile.lastName+' '+Meteor.user().profile.firstName+'!');
+      }
+     });
+  }
+});
