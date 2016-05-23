457122d5481a387b52054d2fdcdd2d99d754c8fc
Talgat Amanbayev <amanbayev@gmail.com>
2016-05-23 16:37:26 +0600

    initial commit lol

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+
+import '../imports/ui/mainTemplate/mainTemplate.js';
+import '../imports/ui/blankPage/blankPage.js';
+import '../imports/ui/leftMenu/leftMenu.js';
+
+import '../imports/ui/authorization/login/login.js';
+import '../imports/ui/authorization/register/register.js';
+
+import './main.html';
---
+++ imports/ui/mainTemplate/mainTemplate.js
@@ -0,0 +1,3 @@
+import {Meteor} from 'meteor/meteor';
+
+import './mainTemplate.html';
